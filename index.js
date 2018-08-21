82cbe4d73e6df3dacd1b77b01434a150f3719844
Neha Chopade <nehachopade22@gmail.com>
2018-08-21 23:10:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-document.querySelector('#main')
+const main = document.querySelector('#main')
 
-const mainElement= document.querySelector('#main')
-
-//mainElement.addEventListener('click',(event)=>{})
-
-mainElement.addEventListener('click',(event) =>{console.log('I was clicked')})
+main.addEventListener('click', (event) =>
+  {console.log('I was clicked')})
