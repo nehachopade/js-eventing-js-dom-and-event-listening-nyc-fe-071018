a85c28130cc24051b6a94167b4ad97ba068aa5c4
Neha Chopade <nehachopade22@gmail.com>
2018-08-21 23:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const main = document.querySelector('#main')
+const paragraph = document.querySelector('p')
 
 main.addEventListener('click', (event) =>
   {   alert('Hey you clicked me')
     
   }
   )
+
+paragraph.addEventListener('click', (event) => { alert ('You clicked a p')})
