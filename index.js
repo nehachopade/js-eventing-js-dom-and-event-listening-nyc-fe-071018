3fff2523015cb1b98f75d642d04a52ea171552d2
Neha Chopade <nehachopade22@gmail.com>
2018-08-22 00:07:12 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+const main = document.querySelector('#main')
+const paragraph = document.querySelector('p')
+const input=document.querySelector('input')
+
+main.addEventListener('click', (event) =>
+  {   alert('Hey you clicked me')
+    
+  }
+  )
+
+input.addEventListener('keydown', (event) =>
+{   if (event.which === 71){
+      event.preventDefault()
+      }
+}
+)
+
+paragraph.addEventListener('click', (event) => 
+{ 
+    alert ('You clicked a p')
+    paragraph.style.backgroundColor="Yellow"
+})
+
+let divs = document.querySelectorAll('div')
+
+function bubble(event) {
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled')
+}
+for (div of divs){
+   div. addEventListener('click',bubble, true)
+}
+
